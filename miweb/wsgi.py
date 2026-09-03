ba013a6c1d7fae49a8c0b4b4bb2110792d1cca57
alejxdev <zambranoconejo8@gmail.com>
2026-09-03 18:43:03 -0500

    Update wsgi.py

--- miweb/wsgi.py
+++ miweb/wsgi.py
@@ -14,3 +14,18 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miweb.settings')
 
 application = get_wsgi_application()
+
+# --- CREACIÓN AUTOMÁTICA DE SUPERUSUARIO AL ARRANCAR ---
+try:
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'alejo1234')
+    else:
+        u = User.objects.get(username='admin')
+        u.set_password('alejo1234')
+        u.is_staff = True
+        u.is_superuser = True
+        u.save()
+except Exception:
+    pass # Evita que la aplicación falle si la base de datos aún no está lista
+---------------------------------------------------------
