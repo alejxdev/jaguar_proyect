f09343b96c0f752415759a113d352449fd961029
alejxdev <zambranoconejo8@gmail.com>
2026-09-03 19:48:43 -0500

    Update settings.py

--- miweb/settings.py
+++ miweb/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Agrégala aquí
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
